c1f3ba8c831d599a695a5218f82a9cccb1cf4b6b
valecambiaso <71225806+valecambiaso@users.noreply.github.com>
2023-05-19 20:41:04 -0300

    font y carousel

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,48 @@
-function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+import Carousel from 'react-bootstrap/Carousel';
+import React,{useEffect, useState} from 'react';
+
+type Queso = {
+    id:bigint;
+    nombre:string;
+    precio_x_kg:number;
+    foto:BinaryData;
+    descripcion:string;
 }
+
+function Home() {
+
+    const[quesos,setQuesos] = useState([])
+   
+    const url = 'https://cambiaso-crozes-laravel-955ex9rx5-cambiaso-crozes.vercel.app/rest/quesos'
+    
+    const showData = async() => {
+        const response = await fetch(url);
+        const dataQuesos = await response.json();
+        console.log(dataQuesos);
+        setQuesos(dataQuesos);
+    }
+
+    useEffect(() => {
+        showData();
+    }, []);
+
+    return (
+        <Carousel className="align-items-center">
+            {quesos.map((queso:Queso) => 
+                <Carousel.Item interval={1000}>
+                <img
+                    className="w-50"
+                    src={`${"data:image/png;base64," + queso.foto}`}
+                    alt={queso.nombre}
+                />
+                <Carousel.Caption>
+                  <h3>{queso.nombre}</h3>
+                  <p>{queso.descripcion}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )}
+        </Carousel>
+    );
+}
+
 export default Home;
--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -1,3 +1,4 @@
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,30 +7,34 @@ import { Outlet, Link } from 'react-router-dom';
 function NavbarEx() {
     return (
         <>
-            <Navbar className="navBg" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand as={Link} to="/">
-                    <img 
-                        src="https://i.ibb.co/4m3QVsf/logo-cut.png"
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top"
-                        alt=""
-                    />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/quesos">Quesos</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
+            <Navbar className="navBg" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img 
+                            src="https://i.ibb.co/4m3QVsf/logo-cut.png"
+                            width="60"
+                            height="60"
+                            className="d-inline-block align-top"
+                            alt=""
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <b><Nav.Link as={Link} to="/">Home</Nav.Link></b>
+                            <b><Nav.Link as={Link} to="/quesos">Quesos</Nav.Link></b>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
 
             <section>
                 <Outlet></Outlet>
             </section>
+
+            <Navbar className="navBg" expand="lg">
+                
+            </Navbar>
         </>
     )
 }
